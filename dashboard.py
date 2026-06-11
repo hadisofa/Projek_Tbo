de48444ac2bfe0df212b4b9ec852dcfca4030b65
Hadisofa <mohammadhadisofa@gmail.com>
2026-06-12 00:35:13 +0700

    perbaikan

--- dashboard.py
+++ dashboard.py
@@ -62,7 +62,7 @@ def dashboard_page():
         """, unsafe_allow_html=True)
     st.image(
         "assets/xiaomi14.png",
-        width=900
+        use_container_width=True
     )
 
     st.markdown("<br>", unsafe_allow_html=True)
